280aff000972112365738c225f9aedab4bf2b14d
muditasisodia <muditasisodia@gmail.com>
2018-08-07 11:18:50 +0530

    Added card matching logic

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 /*
  * Create a list that holds all of your cards
  */
-
+const symbols = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"];
+var matched = [];
+var open = [];
 
 /*
  * Display the cards on the page
@@ -9,6 +11,71 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+ const shuffled = shuffle(symbols);
+
+ //displaying card grid
+ const myDocFrag = document.createDocumentFragment();
+ for(let i = 0; i<shuffled.length; i++) {
+   var card = document.createElement("li");
+   card.className = "card";
+   var symbol = document.createElement("i");
+   symbol.className = shuffled[i];
+   card.appendChild(symbol);
+   myDocFrag.appendChild(card);
+
+   card.addEventListener("click", function() {
+     this.classList.add("show", "open");
+     addToOpen(this);
+   });
+ }
+
+ var deck = document.querySelector(".deck");
+ deck.appendChild(myDocFrag);
+
+ //Adding card to list of open cards
+ function addToOpen(cardToOpen){
+   open.push(cardToOpen);
+   //created a class disabled to ensure that opened cards cannot be reopened and matched to themselves
+   cardToOpen.classList.add("disabled");
+   if(open.length === 2){
+
+     //accessing class of i element within card
+     var newSymbol = open[1].children[0].classList[1];
+     var oldSymbol = open[0].children[0].classList[1];
+
+     if(newSymbol === oldSymbol){
+       matched.push(newSymbol, oldSymbol);
+       matchCard(open[0]);
+       matchCard(cardToOpen);
+       open = [];
+       isgameOver();
+     }
+     else{
+       setTimeout(()=>{
+         closeCard(open[0]);
+         closeCard(cardToOpen);
+         open = [];
+       }, 500);
+
+     }
+   }
+ }
+
+ function matchCard(element){
+   element.classList.add("match");
+ }
+
+
+ function closeCard(element) {
+   element.classList.remove("show", "open", "disabled");
+ }
+
+
+ function isgameOver(){
+   if(symbols.length === matched.length){
+     alert("game over");
+   }
+ }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -24,7 +91,6 @@ function shuffle(array) {
 
     return array;
 }
-
 
 /*
  * set up the event listener for a card. If a card is clicked:
